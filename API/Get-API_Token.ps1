e229fcdfe0ea875cb62cb579eb60b39727ca57f2
jallen-tech <19367613+jallen-tech@users.noreply.github.com>
2021-08-26 18:17:24 -0400

    Added logic for refreshing access tokens

--- API/Get-API_Token.ps1
+++ API/Get-API_Token.ps1
@@ -3,17 +3,83 @@
     Jeff Allen
     im@jeffreyallen.tech
 #>
-$Creds=Get-credentials
-$Username=$Creds.Username
-$Password=$Creds.GetNetworkCredential().Password
-$RequestURL="https://localhost:8080/auth/realms/stigman/protocol/openid-connect/token"
-$body=@{
-    grant_type='password'
-    username=$Username
-    password=$Password
-    client_id='stig-manager'
-    client_secret='client_secret'
+<#
+    .SYNOPSIS
+    This script will get a bearer token from a keycloak based authentication source.
+
+    .DESCRIPTION
+    This script is designed to call a keycloak realm (stigman) and provide the user/script a bearer token to make follow up API calls.
+
+    .PARAMETERS
+    -URL <string>
+        Required?                    True
+
+    -Client_ID <string>
+        Required?                    True
+
+    -Client_Secret <string>
+        Required?                    True
+
+    .OUTPUTS
+    $global:AccessToken is available to the user to run API calls.
+    $global:RefreshToken is available to the user to refresh the Access Token.
+
+    .EXAMPLE
+    PS> .\Get-API_Token -URL https://localhost:54000/auth/realms/stigman -Client_ID stig-manager - Client_Secret ####
+    Will provide an Access and Refresh token.
+
+    .EXAMPLE
+    PS> .\Get-API_Token -RefreshToken $True
+    Will refresh the access token.
+
+
+#>
+
+param(
+    [parameter(Mandatory=$False]
+    [String]$URL,
+    [parameter(Mandatory=$False)]
+    [String]$Client_ID,
+    [parameter(Mandatory=$False)]
+    [String]$Client_Secret,
+    [parameter(Mandatory=$False)]
+    [Bool]$RefreshToken
+)
+
+# Variables (if you dont use the parameters)
+$URL="https://localhost:8080/auth/realms/stigman"
+$Client_ID="stig-manager"
+$Client_Secret=""
+
+if (((Get-Date -UFormat %s) -gt $global:AccessTimer) AND ((Get-Date -UFormat %s) -lt $global:ExpiredTimer)){
+    #Refresh Access Token
+    $URI=$URL+"/protocol/openid-connect/token"
+    $body=@{
+        grant_type='refresh_token'
+        refresh_token=$global:RefreshToken
+        client_id=$Client_ID
+        client_secret=$Client_Secret
+    }
+    $Result=Invoke-RestMethod -Method POST -URI $URI -ContentType "application/x-www-form-urlencoded" -Body $Body -Credentials $Creds    
+    $global:AccessToken=($Results.access_token) | ConvertTo-SecureString - AsPlainText -Force
+    $global:RefreshToken=($Results.refresh_token) | ConvertTo-SecureString - AsPlainText -Force
+    $global:AccessTimer=(Get-Date -UFormat %s)+($Results.expires)
 }
-$Result=Invoke-RestMethod -Method POST -URI $RequestURL -ContentType "application/x-www-form-urlencoded" -Body $Body -Credentials $Creds
-$global:AccessToken=($Results.access_token) | ConvertTo-SecureString - AsPlainText -Force
-$global:RefreshToken=($Results.refresh_token) | ConvertTo-SecureString - AsPlainText -Force
+else {
+    $Creds=Get-credentials
+    $Username=$Creds.Username
+    $Password=$Creds.GetNetworkCredential().Password
+    $URI=$URL+"/protocol/openid-connect/token"
+    $body=@{
+        grant_type='password'
+        username=$Username
+        password=$Password
+        client_id=$Client_ID
+        client_secret=$Client_Secret
+    }
+    $Result=Invoke-RestMethod -Method POST -URI $URI -ContentType "application/x-www-form-urlencoded" -Body $Body -Credentials $Creds
+    $global:AccessToken=($Results.access_token) | ConvertTo-SecureString - AsPlainText -Force
+    $global:RefreshToken=($Results.refresh_token) | ConvertTo-SecureString - AsPlainText -Force
+    $global:AccessTimer=(Get-Date -UFormat %s)+($Results.expires)
+    $global:ExpiredTimer)=[int](Get-Date -UFormat %s)+[int]64800
+}
