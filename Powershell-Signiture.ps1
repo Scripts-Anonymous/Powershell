9f87b12d932fe56d83f1c91e8777dfc54024cab7
jallen-tech <19367613+jallen-tech@users.noreply.github.com>
2021-08-28 12:25:26 -0400

    Added Signiture Example

---
+++ Powershell-Signiture.ps1
@@ -0,0 +1,34 @@
+<#
+    .NAME
+
+
+    .AUTHOR
+    Jeff Allen
+    im@jeffreyallen.tech
+
+    .SYNOPSIS
+    
+
+    .SYNTAX
+
+
+    .DESCRIPTION
+    
+
+    .OUTPUTS
+    
+
+    .RELATED LINKS
+    GITHUB link:
+
+    
+    .REMARKS
+
+
+    .EXAMPLE
+    PS> .\
+
+    .EXAMPLE
+    PS> .\
+
+#>
