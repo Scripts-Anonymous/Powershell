3a8350f1d653aa9f6210bdd7ee48570f5448b8d4
jallen-tech <19367613+jallen-tech@users.noreply.github.com>
2023-02-11 21:22:46 -0500

    changed filename added function

---
+++ Template.ps1
@@ -0,0 +1,39 @@
+<#
+    .NAME
+
+
+    .AUTHOR
+    
+
+    .SYNOPSIS
+
+
+    .DESCRIPTION
+    
+
+    .OUTPUTS
+    
+
+    .RELATED LINKS
+    GITHUB link:
+
+    
+    .REMARKS
+
+
+    .EXAMPLE
+    PS> 
+
+    .EXAMPLE
+    PS> 
+
+#>
+
+#Requires Powershell 5
+
+function FunctionName {
+    param (
+        OptionalParameters
+    )
+    
+}
