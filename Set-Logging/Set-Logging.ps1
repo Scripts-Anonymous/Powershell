d433d5f50b84800523fc8c2d439191edced4366f
jallen-tech <19367613+jallen-tech@users.noreply.github.com>
2023-02-14 21:15:03 -0500

    added logs

--- Set-Logging/Set-Logging.ps1
+++ Set-Logging/Set-Logging.ps1
@@ -6,7 +6,10 @@
     Jeff Allen
 
     .SYNOPSIS
-    This function sets environment variables for script output (ReportOnly) and/or transcripts (TranscriptOnly). You can view the variables by running an "ls env:".
+    This function sets environment variables for logs (LogOnly), script output (ReportOnly), and/or transcripts (TranscriptOnly). You can view the variables by running an "ls env:".
+
+    .DESCRIPTION
+    To enable environment variables please run "Set-Logging" and/or change all scripts to run "Set-Logging" as their first command.
 
     .RELATED LINKS
     GITHUB link:
@@ -19,22 +22,27 @@
 
 #>
 
-#Requires Powershell 5
+#Requires -Version 5
 
 function Set-Logging {
     param (
         [parameter(Mandatory=$False)]
-        [ValidateSet("ReportOnly","TranscriptOnly","Remove")]
+        [ValidateSet("LogOnly","ReportOnly","TranscriptOnly","RemoveAll")]
         [Switch]$Option
     )
 
     # Variables
-    $ReportEnvName = "Report"
+    $LogEnvName = "Logs"
+    $LogPath = "C:\Scripts\Logs"
+    $ReportEnvName = "Reports"
     $ReportPath = "C:\Scripts\Reports"
-    $TranscriptEnvName = "Transcript"
+    $TranscriptEnvName = "Transcripts"
     $TranscriptPath = "C:\Scripts\Transcripts"
 
     Switch ($Option) {
+        "LogOnly"    {
+            New-Item -Path Env: -Name $LogEnvName -Value $LogPath
+        }        
         "ReportOnly"    {
             New-Item -Path Env: -Name $ReportEnvName -Value $ReportPath
         }
@@ -42,10 +50,12 @@ function Set-Logging {
             New-Item -Path Env: -Name $TranscriptEnvName -Value $TranscriptPath
         }
         Default {
+            New-Item -Path Env: -Name $LogEnvName -Value $LogPath
             New-Item -Path Env: -Name $ReportEnvName -Value $ReportPath
             New-Item -Path Env: -Name $TranscriptEnvName -Value $TranscriptPath
         }
-        "Remove"    {
+        "RemoveAll"    {
+            Remove-Item Env:$LogEnvName
             Remove-Item Env:$ReportEnvName
             Remove-Item Env:$TranscriptEnvName
         }
